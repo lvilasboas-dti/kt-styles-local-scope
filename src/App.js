c2f06d396a49e61a0345eae2bd074a6fc0931593
Lucas Vilas Boas <lucas.vilasboas@dtidigital.com.br>
2021-12-01 15:03:39 -0300

    Buttons in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
 
+import { Menu } from 'react-feather';
+
+import { ButtonCssModules } from './components/ButtonCssModules/ButtonCssModules';
+import { ButtonStyledComponents } from './components/ButtonStyledComponents/ButtonStyledComponents';
+
+// green: #28a745
+// red: #dc3545
+
 const App = () => {
     return (
         <div className='app'>
+
+            <div className='button-css-module'>
+                <ButtonCssModules
+                    icon={<Menu />}
+                    label={'Botão CSS Modules'}
+                />
+            </div>
+
+            <div className='button-styled-components'>
+                <ButtonStyledComponents
+                    icon={<Menu />}
+                    label={'Botão Styled Components'}
+                />
+            </div>
             
         </div>
     )
